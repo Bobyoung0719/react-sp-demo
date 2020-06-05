90d95c3da1decda6de3f265f4e5402d27a6219d4
Bobyoung0719 <Bobyoung0719@gmail.com>
2020-06-05 10:22:36 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import loadable from '@loadable/component';
-import React, {Suspense, lazy, createContext} from 'react';
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
-
-=======
 import React, {Suspense, lazy} from 'react';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
->>>>>>> 5bc53c6be969b7a0fed55187fb0aaebe56c14f1a
 import Loading from '$com/Loading';
 import '../html/init.css';
 
@@ -16,40 +9,14 @@ const Index = loadable(() => import(`./Index`));
 
 
 // 第二页
-<<<<<<< HEAD
-const Page2 = loadable(() => import('./Page2'));
-=======
 const Home = lazy(() => import('./Home'));
->>>>>>> 5bc53c6be969b7a0fed55187fb0aaebe56c14f1a
 
 // 第二页
 const Nomatch = loadable(() => import('./Nomatch'));
 
-<<<<<<< HEAD
-const themes = {
-  light: {
-    headColor: '#84c225',
-    backGround: '#f00'
-  },
-
-  dark: {
-    headColor: '#ccc',
-    backGround: '#000'
-  }
-}
-
-export const ThemeContext = createContext(themes);
-
-function App(props) {
-  const baseName = location.host.includes('localhost') ? '' : '/alex-rsp'; 
-
-  console.log('baseName：', baseName)
- 
-=======
 function App() {
   const baseName = location.host.includes('localhost') ? '' : '/alex-rsp'; 
 
->>>>>>> 5bc53c6be969b7a0fed55187fb0aaebe56c14f1a
   return (
     <Router basename={baseName}>
       <Suspense fallback={<Loading visible/>}>
@@ -67,16 +34,11 @@ function App() {
           <Route path="/no-match">
             <Nomatch />
           </Route>
-          {/* <Route path="*">
-            <Nomatch />
-          </Route> */}
         </Switch>
       </Suspense>
     </Router>
   )
 };
-
-// console.log(<App />)
 
 ReactDOM.render(
   <App />,
