ef26e9b0f1b441c192fc1290762736e42f566659
Bobyoung0719 <Bobyoung0719@gmail.com>
2020-06-16 16:22:04 +0800

    update

--- src/Index/index.js
+++ src/Index/index.js
@@ -7,7 +7,7 @@ function Index(props) {
 
   return (
     <div className={styles.container}>
-      <p>—————— 哈哈哈 ————————</p>
+      <p>—————— 哈哈哈 ———————ddddddddddddddddd—</p>
       <button onClick={() => history.push('/home/1')}>next page</button>
       <button onClick={() => history.push('/no-match')}>no-match</button>
     </div>
